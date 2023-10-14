6da19a369d89c009783e5b560c9a620bffadaf9f
denji <90654120+denji6969@users.noreply.github.com>
2023-10-14 21:29:54 +0600

    Add files via upload

---
+++ plugins/message_logger/update.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    version: 'End Of Life (NO MAJOR UPDATE)',
+    patch: [
+      'This plugin will not be updated anymore in term MAJOR UPDATE, until further infos.',
+    ]
+  },
+  {
+    version: 'v0.1.3',
+    patch: [
+      "Fixed Crashing Issue when Embedded Links got Sent on Chat",
+      "Fixed Crashing Issue When Long Press a Message",
+      "Fixed Crashing Issue when Opening Plugin Settings",
+    ]
+  },
+  {
+    version: 'v0.0.8 - v0.0.82',
+    patch: [
+      'Added minimalistic option',
+      'Added remove edited log history when long press a message.',
+      'Added Ignore user list, by UserID and Name.',
+      'Added Customizeable Text Color for Deleted Messages.',
+      'Added Customizeable Background Color for Deleted Messages.',
+      'Added Customization for Edited Messages Separator.',
+      'Fixed for when editing a own message it only take the last edited history rather than everything.',
+      'Fixed Message fails to logs. (lmk if persist).',
+    ]
+  } 
+]
