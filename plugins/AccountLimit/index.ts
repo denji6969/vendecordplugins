a362e723aff2f5d956323d5763a331da69634c76
denji <90654120+denji6969@users.noreply.github.com>
2023-11-07 18:28:39 +0600

    Update index.ts

--- plugins/AccountLimit/index.ts
+++ plugins/AccountLimit/index.ts
@@ -5,14 +5,14 @@ const accounts = findByProps("MAX_ACCOUNTS")
 const DEFAULT_VALUE = 5
 
 export const onLoad = () => {
-    accounts.MAX_ACCOUNTS = 100
+    accounts.MAX_ACCOUNTS = 1000
 }
 
 export const onUnload = () => showConfirmationAlert({
     title: "Hold on",
     content: `Disabling plugin will *remove* accounts to ${DEFAULT_VALUE} (default). Are you sure?`,
     onConfirm: () => accounts.MAX_ACCOUNTS = DEFAULT_VALUE,
-    confirmText: "YES",
+    confirmText: "Yes",
     cancelText: "Cancel",
     isDismissable: false
 })
